e59fcfa5c8d1ba3c20982237e816064fc8a01417
Avijeet Pandey <avijeetpandey25@gmail.com>
2025-12-15 20:49:52 +0530

    fix: DB commands

--- include/db.h
+++ include/db.h
@@ -15,7 +15,27 @@ struct Book {
   std::optional<std::string> due_date; // ISO string
 };
 
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Book, id, title, author, isbn, assigned_to, due_date)
+inline void to_json(nlohmann::json &j, const Book &b) {
+  j = nlohmann::json{
+    {"id", b.id},
+    {"title", b.title},
+    {"author", b.author},
+    {"isbn", b.isbn}
+  };
+  if (b.assigned_to) j["assigned_to"] = *b.assigned_to; else j["assigned_to"] = nullptr;
+  if (b.due_date) j["due_date"] = *b.due_date; else j["due_date"] = nullptr;
+}
+
+inline void from_json(const nlohmann::json &j, Book &b) {
+  if (j.contains("id") && !j["id"].is_null()) b.id = j["id"].get<int>();
+  if (j.contains("title") && !j["title"].is_null()) b.title = j["title"].get<std::string>();
+  if (j.contains("author") && !j["author"].is_null()) b.author = j["author"].get<std::string>();
+  if (j.contains("isbn") && !j["isbn"].is_null()) b.isbn = j["isbn"].get<std::string>();
+  if (j.contains("assigned_to") && !j["assigned_to"].is_null()) b.assigned_to = j["assigned_to"].get<std::string>();
+  else b.assigned_to.reset();
+  if (j.contains("due_date") && !j["due_date"].is_null()) b.due_date = j["due_date"].get<std::string>();
+  else b.due_date.reset();
+}
 
 class DB {
 public:
